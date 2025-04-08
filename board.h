2317bceb8d997c57fd2b30dd497a62f7040f7b3c
d0lb <pavelkislenko1@gmail.com>
2025-04-08 07:25:55 +0300

    made a first playable version

--- board.h
+++ board.h
@@ -1,8 +1,19 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include <stdio.h>
+#include <stdlib.h>
 
-void displayBoard(char board[3][3]){
+void clearTerminal(){
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+
+void displayBoard(const char board[3][3]){
+    clearTerminal();
     printf("\n");
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
@@ -12,16 +23,39 @@ void displayBoard(char board[3][3]){
     }
 }
 
-char checkWinConditions(char board[3][3]){
-    if(board[1][0] == board[1][1] && board[1][0] == board[1][2]){
-        return board[1][0];
-    }else if (board[0][0] == board[0][1] && board[0][0] == board[0][2]) {
-        return  board[0][0];
-    }else if (board[2][0] == board[2][1] && board[2][0] == board[2][2]) {
-        return board[2][0];
-    }else if (board[]) {
-    
+void setASymbol(char board[3][3], int num1, int num2, char symbol){
+    board[num1][num2] = symbol;
+}
+
+char checkWinConditions(const char board[3][3], const int steps){
+    //string check
+    for(int i = 0; i < 3; i++){
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != ' '){
+            return board[i][0];
+        }
     }
+
+    //columns check
+    for(int i = 0; i < 3; i++){
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != ' '){
+            return board[0][i];
+        }
+    }
+
+    //diagonal check
+
+    if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != ' ') {
+        return board[0][0];
+    }
+    if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != ' ') {
+        return board[0][2];
+    }
+
+    if(steps == 9){
+        return 'D'; //Draw
+    }
+
+    return 'N'; //game continues
 }
 
 #endif
--- main.c
+++ main.c
@@ -3,21 +3,53 @@
 #include <stdbool.h>
 
 bool isRunning = true;
-char board[3][3] ={{'X', 'O', 'X'}};
+char board[3][3];
 
 int num1, num2;
-
+int steps;
 int main(){
     printf("Welcome to multiplayer console TicTacToe Game!");
     while (isRunning){
-        printf("Starting the game... 
-            \n You both will have to enter two numbers,
-            that means coordinates of a TicTacToe board
-            (Example: 2 2 --- makes a symbol 
-            in the center) \n");
-            while(1){
-                printf("X's turn!");
-                scanf
+        steps = 0;
+        printf("Starting the game... \n You both will have to enter two numbers,that means coordinates of a TicTacToe board (Example: 1 1 --- makes a symbol in the center) \n");
+            
+            while(steps < 9){
+
+                printf("X's turn! \n");
+                scanf(" %d %d", &num1, &num2);
+                setASymbol(board, num1, num2, 'X');
+                steps++;
+                displayBoard(board);
+                if(checkWinConditions(board, steps) == 'X'){
+                    printf("X is a winner! \n ");
+                    break;
+                }else if(checkWinConditions(board, steps) == 'O'){
+                    printf("O is a winner! \n ");
+                    break;
+                }else if(checkWinConditions(board, steps) == 'D'){
+                    printf("Draw! \n ");
+                    break;
+                }
+                //#################################
+
+                printf("O's turn! \n");
+                scanf(" %d %d", &num1, &num2);
+                setASymbol(board, num1, num2, 'O');
+                steps++;
+                displayBoard(board);
+
+                
+                if(checkWinConditions(board, steps) == 'X'){
+                    printf("X is a winner! \n ");
+                    break;
+                }else if(checkWinConditions(board, steps) == 'O'){
+                    printf("O is a winner! \n ");
+                    break;
+                }else if(checkWinConditions(board, steps) == 'D'){
+                    printf("Draw! \n ");
+                    break;
+                }
+                
             }
     }
 
